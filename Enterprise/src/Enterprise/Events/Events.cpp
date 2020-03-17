0fa099069d7f07261a5030041160c6e1ef67017e
Michael Martz <michael.martz@live.com>
2020-03-17 18:56:59 -0500

    Added subscription functions.  Identified a problem with static registration functions which I plan to address next.

--- Enterprise/src/Enterprise/Events/Events.cpp
+++ Enterprise/src/Enterprise/Events/Events.cpp
@@ -1,12 +1,17 @@
 #include "EP_PCH.h"
 #include "Events.h"
 
-// Singletons -------------------------------------------------------------------------------------
+// Singletons =========================================================================================================
 // Note: these MUST be singletons, as they are invoked during static initialization.
 
 // Vector associating EventCategorys to vectors of EventTypes.
 std::vector<std::vector<EventType>>& _categoryMap() {
 	static auto instance = new std::vector<std::vector<EventType>>();
+	return *instance;
+}
+std::vector<std::vector<Enterprise::Events::EventCallbackPtr>>& Enterprise::Events::_callbackPtrs()
+{
+	static auto instance = new std::vector<std::vector<Enterprise::Events::EventCallbackPtr>>();
 	return *instance;
 }
 #ifdef EP_CONFIG_DEBUG
@@ -22,9 +27,11 @@ std::vector<const char*>& _typeDebugNames() {
 }
 #endif
 
+bool operator == (const EventType& left, const EventType& right) { return left.m_ID == right.m_ID; }
+
 namespace Enterprise
 {
-	// Registration functions ---------------------------------------------------------------------
+	// Static Type and Category Registration =============================================================================
 
 	#ifndef EP_CONFIG_DEBUG
 	EventCategory Events::RegisterCategory()
@@ -37,7 +44,7 @@ namespace Enterprise
 		EventCategory returnVal = count++;
 
 		// Add empty category to _categoryMap
-		_categoryMap().emplace_back(std::vector<EventType>());
+		_categoryMap().emplace_back();
 
 		// Add debug name to _categoryDebugNames
 		#ifdef EP_CONFIG_DEBUG
@@ -57,6 +64,9 @@ namespace Enterprise
 		static unsigned int count = 0;
 		EventType returnVal = count++;
 
+		// Create new callback stack
+		_callbackPtrs().emplace_back();
+
 		// Add type to appropriate _categoryMap categories
 		for (auto it = categories.m_IDs.begin(); it != categories.m_IDs.end(); ++it)
 			_categoryMap().at(*it).emplace_back(returnVal);
@@ -69,7 +79,7 @@ namespace Enterprise
 		return returnVal;
 	}
 
-	// Debug string getters -----------------------------------------------------------------------
+	// Debug string getters ===========================================================================================
 	#ifdef EP_CONFIG_DEBUG
 	std::string Events::GetCategoryDebugNames(EventCategory category)
 	{
@@ -90,7 +100,7 @@ namespace Enterprise
 
 	const char* Events::GetTypeDebugName(EventType type)
 	{
-		try { return _typeDebugNames().at(type.m_ID); }
+		try { return _typeDebugNames().at(type.ID()); }
 		catch (std::out_of_range)
 		{
 			EP_ERROR("Error: Events::getTypeDebugName() passed unregistered EventType.");
@@ -98,4 +108,33 @@ namespace Enterprise
 		}
 	}
 	#endif
+
+	// Event subscription functions ===================================================================================
+
+	void Events::SubscribeToType(EventType type, EventCallbackPtr callback)
+	{
+		// For every callback already registered for this type...
+		for (auto it = _callbackPtrs().at(type.ID()).begin(); it != _callbackPtrs().at(type.ID()).end(); ++it)
+		{
+			// ...check that it isn't equal to the new callback.
+			if ((*it) == callback)
+			{
+				//TODO: Find a way to turn off warnings for this from the client.
+				EP_WARN("Events: An attempt was made to register the same callback for a single EventType twice.\n"
+					"Are you attempting to subscribe to overlapping event categories? Type:{}", type);
+				return;
+			}
+		}
+		_callbackPtrs().at(type.ID()).emplace_back(callback);
+	}
+	void Events::SubscribeToCategories(EventCategory category, EventCallbackPtr callback)
+	{
+		// For every category requested...
+		for (auto it = category.m_IDs.begin(); it != category.m_IDs.end(); ++it)
+		{
+			// ...subscribe to the types within.
+			for (auto typeIt = _categoryMap().at(*it).begin(); typeIt != _categoryMap().at(*it).end(); ++typeIt)
+				SubscribeToType(*typeIt, callback);
+		}
+	}
 }
--- Enterprise/src/Enterprise/Events/Events.h
+++ Enterprise/src/Enterprise/Events/Events.h
@@ -2,14 +2,14 @@
 #include "EP_PCH.h"
 #include "Core.h"
 
-// Types
+// Types ==============================================================================================================
 struct EventCategory //A category, or combination of categories, for Enterprise events.
 {
 	std::vector <unsigned int> m_IDs; //List of event category IDs represented by this EventCategory object.
 	EventCategory(unsigned int ID) { m_IDs.emplace_back(ID); } //Standard constructor
 
 	//Gets an empty EventCategory object.
-	static EventCategory None() { return EventCategory(); } 
+	static EventCategory None() { return EventCategory(); }
 
 	//Return an EventCategory containing all of the IDs of both EventCategory objects.
 	EventCategory operator | (const EventCategory& other) const
@@ -20,15 +20,20 @@ struct EventCategory //A category, or combination of categories, for Enterprise 
 		return returnVal;
 	}
 private:
-	EventCategory() {} //A parameter-less constructor represents a null EventCategory.
+	EventCategory() {} //An empty EventCategory indicates no category assignment to a type.
 };
 struct EventType  //An Enterprise event type.
 {
-	unsigned int m_ID;
+public:
 	EventType(unsigned int ID) : m_ID(ID) {}
+	unsigned int ID() { return m_ID; }
+	friend bool operator == (const EventType& left, const EventType& right);
+private:
+	unsigned int m_ID;
 };
+bool operator == (const EventType& left, const EventType& right);
 
-// Type and Category Registration Macros
+// Static Type and Category Registration Macros =======================================================================
 #ifdef EP_CONFIG_DEBUG // -----------------------
 // Define a new Enterprise event category.
 #define EP_EVENTCATEGORY(name) namespace EventCategories { const EventCategory name = Enterprise::Events::RegisterCategory(#name); }
@@ -41,6 +46,7 @@ struct EventType  //An Enterprise event type.
 #define EP_EVENTTYPE(name, categories) namespace EventTypes { const EventType name = Enterprise::Events::RegisterType(categories); }
 #endif // ---------------------------------------
 
+// System classes =====================================================================================================
 namespace Enterprise
 {
 	/* Events
@@ -102,16 +108,16 @@ namespace Enterprise
 
 		// Type registration functions
 
-		#ifdef EP_CONFIG_DEBUG
-		// Registers a new event category, and returns its associated bitfield.
-		static EventCategory RegisterCategory(const char* debugName);
-		// Registers a new event type, and returns its associated ID.
-		static EventType RegisterType(const char* debugName, EventCategory categories = EventCategory::None());
-		#else
+		#ifndef EP_CONFIG_DEBUG
 		// Registers a new event category, and returns its associated bitfield.
 		static EventCategory RegisterCategory();
 		// Registers a new event type, and returns its associated ID.
 		static EventType RegisterType(EventCategory categories = EventCategory::None());
+		#else
+		// Registers a new event category, and returns its associated bitfield.
+		static EventCategory RegisterCategory(const char* debugName);
+		// Registers a new event type, and returns its associated ID.
+		static EventType RegisterType(const char* debugName, EventCategory categories = EventCategory::None());
 		#endif
 
 		// Debug name lookup
@@ -125,28 +131,61 @@ namespace Enterprise
 
 
 		// ----------------------------------------------------------------------------------------
-		// Event callback registrastion functions
+		// Event broadcast and callback functions
 
+		// Typedefs
+		typedef std::shared_ptr<Event> EventPtr; // A smart pointer to an instance of an Enterprise event.
+		typedef bool(*EventCallbackPtr)(EventPtr); // A pointer to a callback function.
 
-		// broadcast?
-		// subscribe to type
-		// subscribe to category
+		// Register a function to be called when a particular type of event occurs.
+		static void SubscribeToType(EventType type, EventCallbackPtr callback);
+		// Register a function to be called when an event belonging to a particular category occurs.
+		static void SubscribeToCategories(EventCategory category, EventCallbackPtr callback);
+
+		// Broadcast an event.  It will be dispatched to subscribers in order of priority.
+		static void Broadcast(EventType type) 
+		{
+			// Generate the event
+			EventPtr e = std::make_shared<Event>(type);
+
+			// Call each registered callback until one returns true
+			for (auto callbackit = _callbackPtrs().at(type.ID()).rbegin(); callbackit != _callbackPtrs().at(type.ID()).rend(); ++callbackit) {
+				if ((*callbackit)(e))
+					break;
+			}
+		}
+		// Broadcast an event with a data payload.  It will be dispatched to subscribers in order of priority.
+		template <typename T>
+		static void Broadcast(EventType type, T data)
+		{
+			// Generate the event
+			EventPtr e = std::make_shared<DataEvent<T>>(type, data);
+
+			// Call each registered callback until one returns true
+			for (auto callbackit = _callbackPtrs().at(type.ID()).rbegin(); callbackit != _callbackPtrs().at(type.ID()).rend(); ++callbackit) {
+				if ((*callbackit)(e))
+					break;
+			}
+		}
+	private:
+		// (Singleton) Vector of callback pointer lists.  Index-aligned to registered EventType underlying values.
+		static std::vector<std::vector<EventCallbackPtr>>& _callbackPtrs();
 	};
 }
 
-// EventPtr typedef
-typedef std::shared_ptr<Enterprise::Events::Event> EventPtr; // A smart pointer to an instance of an Enterprise event.
+// ====================================================================================================================
 
 // Extract data from an Enterprise EventPtr.
 template <typename T>
-T& GetEventData(EventPtr e) {
-	EP_ASSERT(auto converted = std::dynamic_pointer_cast<Enterprise::Events::DataEvent<T>>(e));
+T& GetEventData(Enterprise::Events::EventPtr e) {
+	auto converted = std::dynamic_pointer_cast<Enterprise::Events::DataEvent<T>>(e);
+	EP_ASSERT(converted); //Checks for failed cast.
 	return converted->Data();
 }
 
 // Logging helpers
 #ifdef EP_CONFIG_DEBUG
-inline std::ostream& operator << (std::ostream& os, EventPtr e) { return os << e->ToString(); }
+inline std::ostream& operator << (std::ostream& os, Enterprise::Events::EventPtr e) { return os << e->ToString(); }
 inline std::ostream& operator << (std::ostream& os, EventCategory category) { return os << Enterprise::Events::GetCategoryDebugNames(category); }
 inline std::ostream& operator << (std::ostream& os, EventType type) { return os << Enterprise::Events::GetTypeDebugName(type); }
 #endif
