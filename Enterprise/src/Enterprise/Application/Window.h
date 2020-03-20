fb1c18fb913a950a7eb8f57d5b6d00589c4be339
Michael Martz <michael.martz@live.com>
2020-03-19 23:37:37 -0500

    Improved readability of Window.h, and restructured WindowSettings so title is more optional than width and height.

--- Enterprise/src/Enterprise/Application/Window.h
+++ Enterprise/src/Enterprise/Application/Window.h
@@ -4,33 +4,31 @@
 
 namespace Enterprise
 {
-	/* Window
-		The class that represents the game window.  It is created in Game::Init() and destroyed in Game::Cleanup().
-	*/
+	/* The interface to the game window.  Contains functions for creating, destroying, and manipulating the window. */
 	class Window
 	{
 	public:
-		/* Struct containing the game window's properties. */
+		/* Struct containing window properties. */
 		struct WindowSettings
 		{
-			std::wstring Title;
 			unsigned int Width;
 			unsigned int Height;
+			std::wstring Title;
 			// TODO: Add window icon support
 			// TODO: Add full-screen support
 			// TODO: Add V-sync support
 
 			WindowSettings(
-				const std::wstring& title = L"Enterprise",
 				unsigned int width = 1280,
-				unsigned int height = 720)
-				: Title(title), Width(width), Height(height) {}
+				unsigned int height = 720,
+				const std::wstring& title = L"Enterprise")
+				: Width(width), Height(height), Title(title) {}
 		};
 
-		// Setters ------------------------------------------------------------------
+		// Setters
 		//virtual void SetWindowSettings(WindowSettings & newSettings); TODO: Add ability to update window settings
 
-		// Getters ------------------------------------------------------------------
+		// Getters
 		// Gets the width, in pixels, of the game window's viewing area.
 		static unsigned int GetWidth() { return m_Instance->m_Settings.Width; };
 		// Gets the height, in pixels, of the game window's viewing area.
@@ -40,11 +38,11 @@ namespace Enterprise
 		static Window* Create(const WindowSettings& attributes = WindowSettings());
 		// Destroys the game window.
 		static void Destroy() { EP_ASSERT(m_Instance); delete m_Instance; };
-		virtual ~Window() {}
 
+		virtual ~Window() {}
 	protected:
 		static Window* m_Instance; // The singleton instance.
-		WindowSettings m_Settings; // The window properties.
+		WindowSettings m_Settings; // The window's current properties.
 		Window(const WindowSettings& settings) : m_Settings(settings) {}; // Marked protected for singleton purposes
 	};
 }
