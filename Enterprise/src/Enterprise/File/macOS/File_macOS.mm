b91ba9fbcbdb94b1d74b465ae071298fa185d9d4
Michael Martz <michael.martz@live.com>
2020-12-08 20:42:28 -0600

    Added macOS content and data default directories, except for the temp directory.

---
+++ Enterprise/src/Enterprise/File/macOS/File_macOS.mm
@@ -0,0 +1,44 @@
+#include "EP_PCH.h"
+#if defined(__APPLE__) && defined(__MACH__)
+
+#include "Enterprise/File/File.h"
+
+void Enterprise::File::SetPlatformContentPath()
+{
+	CFBundleRef bundle = CFBundleGetMainBundle();
+	EP_ASSERT(bundle);
+	CFURLRef contentURL = CFBundleCopyResourcesDirectoryURL(bundle);
+	EP_ASSERT(contentURL);
+	UInt8 contentPath[1025];
+	EP_VERIFY(CFURLGetFileSystemRepresentation(contentURL, true, contentPath, 1024));
+
+	contentDirPath = std::string((char*)contentPath) + '/';
+
+	CFRelease(contentURL);
+	CFRelease(bundle);
+}
+
+void Enterprise::File::SetPlatformDataPaths()
+{
+	@autoreleasepool
+	{
+		// Save is a subfolder of this location.
+		NSURL* userPathURL = [[NSFileManager defaultManager] URLForDirectory:NSApplicationSupportDirectory
+																	inDomain:NSUserDomainMask
+														   appropriateForURL:nil
+																	  create:YES
+																	   error:nil];
+		NSURL* machinePathURL = [[NSFileManager defaultManager] URLForDirectory:NSApplicationSupportDirectory
+																	   inDomain:NSLocalDomainMask
+															  appropriateForURL:nil
+																		 create:YES
+																		  error:nil];
+
+		userDirPath = std::string(userPathURL.path.UTF8String) + '/' + Constants::DeveloperName + '/' + Constants::AppName + '/';
+		saveDirPath = userDirPath + "save/";
+		machineDirPath = std::string(machinePathURL.path.UTF8String) + '/' + Constants::DeveloperName + '/' + Constants::AppName + '/';
+		tempDirPath = "Plz replace with real path, soon, mmkay?"; // TODO: Either implement temp path or remove from VFS.
+	}
+}
+
+#endif // macOS
