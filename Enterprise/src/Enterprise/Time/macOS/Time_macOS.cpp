f2724c155fca9448b174559db9ab50f8de56d1e0
Michael Martz <michael.martz@live.com>
2021-10-08 15:44:31 -0700

    Implemented new Time API on Mac

--- Enterprise/src/Enterprise/Time/macOS/Time_macOS.cpp
+++ Enterprise/src/Enterprise/Time/macOS/Time_macOS.cpp
@@ -2,27 +2,33 @@
 #if defined(__APPLE__) && defined(__MACH__)
 
 #include "Core.h"
-#include "Enterprise/Time/Time.h"
+#include "../Time.h"
 
-uint64_t _startCount, _currentCount = {};
-double _tickPeriod = {};
+using Enterprise::Time;
 
-void Enterprise::Time::Init()
+static uint64_t startTicks, currentTicks;
+static mach_timebase_info_data_t timebase = { 0 };
+
+void Time::PlatformInit()
 {
-    // Calculate the tick length in seconds
-    mach_timebase_info_data_t timebase = { 0 };
     EP_VERIFY_EQ(mach_timebase_info(&timebase), KERN_SUCCESS);
-    _tickPeriod = 1e-9 * (double)timebase.numer / (double)timebase.denom;
-
-    // Track what time the program started
-    _startCount = mach_absolute_time();
-    _currentCount = _startCount;
+	startTicks = mach_absolute_time();
 }
 
-float Enterprise::Time::GetRawTime()
+uint64_t Time::GetRawTicks()
 {
-    _currentCount = mach_absolute_time();
-    return float((_currentCount - _startCount) * _tickPeriod);
+	currentTicks = mach_absolute_time();
+	return currentTicks - startTicks;
+}
+
+uint64_t Time::SecondsToTicks(double seconds)
+{
+	return seconds * (double)timebase.denom / (double)timebase.numer / 1e-9;
+}
+
+float Time::TicksToSeconds(uint64_t ticks)
+{
+	return (double)ticks * (double)timebase.numer * 1e-9 / (double)timebase.denom;
 }
 
 #endif
