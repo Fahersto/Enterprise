e483a597ed2a15dd902bee6b496b31571106b886
Michael Martz <michael.martz@live.com>
2021-10-08 11:51:23 -0700

    Removed double buffering on Win32 windowed mode

--- Enterprise/src/Enterprise/Graphics/Win32/Window_Win32.cpp
+++ Enterprise/src/Enterprise/Graphics/Win32/Window_Win32.cpp
@@ -127,7 +127,7 @@ void Window::CreatePrimaryWindow()
 	memset(&pfd, 0, sizeof(pfd));
 	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
 	pfd.nVersion = 1;
-	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL;// | PFD_DOUBLEBUFFER;
 	pfd.iPixelType = PFD_TYPE_RGBA;
 	pfd.cColorBits = 32;
 	pfd.cDepthBits = 24;
